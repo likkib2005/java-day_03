5c3d3a77db100cca27cae9b7e2c7407f56cdfde6
likkib2005 <likkilikitha214@gmail.com>
2024-12-04 10:59:16 +0530

    Add files via upload

---
+++ setbit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class setbit
+{
+public static void main(String args[])
+{
+int a=10;
+int pos=1;
+if((a&(1<<pos))!=0)
+{
+System.out.println("  set bit"); 
+}
+else
+{
+System.out.println(" not  set bit");   
+}
+}
+}
